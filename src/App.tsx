13af912ffe8ba5b49d1f4702a72814f8e8f1c061
makoks <kolesoks@gmail.com>
2022-01-24 11:55:28 +0500

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Layout } from 'antd';
+
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+import './App.css';
 
 import Home from './components/home';
 
+const { Header, Content } = Layout;
+
 function App() {
   return (
-    <div>
-      <h1>Best Friend - Veterinary Clinic</h1>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <Layout>
+      <Header>Best Friend - Veterinary Clinic</Header>
+      <Content>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </Content>
+    </Layout>
   );
 }
 
